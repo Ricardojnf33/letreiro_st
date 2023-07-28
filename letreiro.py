a8c543fa454af85e6f98d93787ac7eff5b0136f1
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-07-28 15:13:55 -0300

    Update letreiro.py

--- letreiro.py
+++ letreiro.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import time
+from st_html import marquee
 
 def main():
     st.title("Letreiro Eletrônico - Estilo Neon")
@@ -16,20 +16,17 @@ def main():
 
         # Verifica se a mensagem foi digitada
         if mensagem:
-            while True:
-                # Exibe a mensagem com estilo Neon
-                st.markdown(
-                    f"<p style='font-size: 32px; color: {cor_neon}; text-shadow: 0 0 10px {cor_neon}, 0 0 20px {cor_neon}, 0 0 30px {cor_neon}, 0 0 40px {cor_neon}, 0 0 50px {cor_neon};'>{mensagem}</p>",
-                    unsafe_allow_html=True
-                )
-                
-                # Faz uma pausa para criar o efeito de movimento
-                time.sleep(0.5)
+            # Exibe a mensagem com estilo Neon e efeito de movimento lateral
+            st.markdown(
+                f"<p style='font-size: 32px; color: {cor_neon}; text-shadow: 0 0 10px {cor_neon}, 0 0 20px {cor_neon}, 0 0 30px {cor_neon}, 0 0 40px {cor_neon}, 0 0 50px {cor_neon};'>"
+                f"<marquee behavior='scroll' direction='left'>{mensagem}</marquee>"
+                "</p>",
+                unsafe_allow_html=True
+            )
 
-                # Limpa a tela antes de exibir a mensagem novamente
-                st.empty()
-        else:
-            st.warning("Por favor, digite uma mensagem para exibir.")
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
