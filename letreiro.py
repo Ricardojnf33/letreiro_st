4240a5ae7ee971e5619217c0f98ac9b6eec0f62a
Ricardo Fernandes <67656688+Ricardojnf33@users.noreply.github.com>
2023-07-28 15:11:54 -0300

    Update letreiro.py

--- letreiro.py
+++ letreiro.py
@@ -2,10 +2,14 @@ import streamlit as st
 import time
 
 def main():
-    st.title("Letreiro Eletrônico")
+    st.title("Letreiro Eletrônico - Estilo Neon")
+    st.write("Criando um efeito de Neon simulado usando Streamlit.")
 
     # Campo de texto para inserir a mensagem
-    mensagem = st.text_input("Surfe conosco a onda super gigante da Revolução da Inteligência Artificial:")
+    mensagem = st.text_input("Digite a mensagem que deseja exibir:")
+
+    # Escolha da cor Neon
+    cor_neon = st.selectbox("Escolha uma cor Neon:", ["#ff0000", "#00ff00", "#0000ff", "#ff00ff", "#ffff00"])
 
     # Botão para iniciar o letreiro
     if st.button("Iniciar Letreiro"):
@@ -13,8 +17,11 @@ def main():
         # Verifica se a mensagem foi digitada
         if mensagem:
             while True:
-                # Exibe a mensagem
-                st.write(mensagem)
+                # Exibe a mensagem com estilo Neon
+                st.markdown(
+                    f"<p style='font-size: 32px; color: {cor_neon}; text-shadow: 0 0 10px {cor_neon}, 0 0 20px {cor_neon}, 0 0 30px {cor_neon}, 0 0 40px {cor_neon}, 0 0 50px {cor_neon};'>{mensagem}</p>",
+                    unsafe_allow_html=True
+                )
                 
                 # Faz uma pausa para criar o efeito de movimento
                 time.sleep(0.5)
